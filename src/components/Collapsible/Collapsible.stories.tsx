bb02b4022bb330d2ee42ed866baad75a25f23e97
Tai Porto <moonkoala23@gmail.com>
2023-11-07 18:31:15 -0300

    chore: fix storybook components path

--- src/components/Collapsible/Collapsible.stories.tsx
+++ src/components/Collapsible/Collapsible.stories.tsx
@@ -5,7 +5,7 @@ import { Collapsible } from "./index";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: "Example/Collapsible",
+  title: "Components/Collapsible",
   component: Collapsible,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
--- src/components/TreeView/TreeView.stories.ts
+++ src/components/TreeView/TreeView.stories.ts
@@ -4,7 +4,7 @@ import { TreeView } from "./index";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: "Example/TreeView",
+  title: "Components/TreeView",
   component: TreeView,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
