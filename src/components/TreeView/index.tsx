0005a31ccd94d56ec2a69c63a762fb3ab151ce27
Tai Porto <tai.porto@pipefy.com>
2023-11-11 13:43:13 -0300

    refactor: move util functions to separate file

--- src/components/TreeView/index.tsx
+++ src/components/TreeView/index.tsx
@@ -2,129 +2,15 @@ import React, { ChangeEvent, useEffect, useState } from "react";
 
 import { Node } from "./components/Node";
 import { NodeData } from "./types";
+import {
+  findNode,
+  handleNodeWasChecked,
+  handleNodeWasIndeterminate,
+  handleNodeWasUnchecked,
+} from "./utils";
 
 export type TreeViewProps = {
   data: NodeData[];
-};
-
-const findNode = (
-  nodeId: NodeData["id"],
-  tree: NodeData[]
-): NodeData | undefined => {
-  let foundNode;
-  if (tree == null) return;
-
-  if (foundNode) {
-    return foundNode;
-  }
-
-  for (const node of tree) {
-    if (node.id === nodeId) {
-      return node;
-    }
-    if (node.childrenNodes) {
-      foundNode = findNode(nodeId, node.childrenNodes);
-      if (foundNode) return foundNode;
-    }
-  }
-};
-
-const cloneForest = (forest: NodeData[]): NodeData[] => {
-  return forest.map((tree) => ({
-    ...tree,
-    childrenNodes: tree.childrenNodes
-      ? cloneForest(tree.childrenNodes)
-      : undefined,
-  }));
-};
-
-const handleNodeWasIndeterminate = (
-  nodeId: NodeData["id"],
-  tree: NodeData[]
-) => {
-  const newTree = cloneForest(tree);
-  const node = findNode(nodeId, newTree);
-
-  if (node) {
-    node.checked = false;
-    node.indeterminate = false;
-
-    node.childrenNodes!.forEach((node) => {
-      node.checked = false;
-      node.indeterminate = false;
-    });
-  }
-  return newTree;
-};
-
-const handleNodeWasChecked = (nodeId: NodeData["id"], tree: NodeData[]) => {
-  const newTree = cloneForest(tree);
-  const node = findNode(nodeId, newTree);
-
-  if (node) {
-    node.checked = true;
-
-    if (node.childrenNodes) {
-      node.childrenNodes.forEach((node) => {
-        node.checked = true;
-        node.indeterminate = false;
-      });
-    }
-
-    if (node.parentId) {
-      const nodeParent = findNode(node.parentId, newTree);
-
-      const nodeSiblings = nodeParent?.childrenNodes!.filter(
-        (node) => node.id !== nodeId
-      );
-      const uncheckedSiblings = nodeSiblings?.filter((node) => node.checked);
-
-      if (nodeSiblings?.length === uncheckedSiblings?.length) {
-        nodeParent!.checked = true;
-        nodeParent!.indeterminate = false;
-      } else {
-        nodeParent!.checked = undefined;
-        nodeParent!.indeterminate = true;
-      }
-    }
-  }
-
-  return newTree;
-};
-
-const handleNodeWasUnchecked = (nodeId: NodeData["id"], tree: NodeData[]) => {
-  const newTree = cloneForest(tree);
-  const node = findNode(nodeId, newTree);
-
-  if (node) {
-    node.checked = false;
-
-    if (node.childrenNodes) {
-      node.childrenNodes.forEach((node) => {
-        node.checked = false;
-        node.indeterminate = false;
-      });
-    }
-
-    if (node.parentId) {
-      const nodeParent = findNode(node.parentId, newTree);
-
-      const nodeSiblings = nodeParent?.childrenNodes!.filter(
-        (node) => node.id !== nodeId
-      );
-      const checkedSiblings = nodeSiblings?.filter((node) => !node.checked);
-
-      if (nodeSiblings?.length === checkedSiblings?.length) {
-        nodeParent!.checked = false;
-        nodeParent!.indeterminate = false;
-      } else {
-        nodeParent!.checked = undefined;
-        nodeParent!.indeterminate = true;
-      }
-    }
-  }
-
-  return newTree;
 };
 
 export const TreeView = ({ data }: TreeViewProps) => {
