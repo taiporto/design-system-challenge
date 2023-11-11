acdc4bb8bf87f0e921f7051cce845cc55b6d5c5b
Tai Porto <tai.porto@pipefy.com>
2023-11-11 13:36:36 -0300

    fix: add aria-disabled to checkbox input

--- src/components/Checkbox/components/CheckboxInput.tsx
+++ src/components/Checkbox/components/CheckboxInput.tsx
@@ -3,5 +3,13 @@ import React, { InputHTMLAttributes } from "react";
 export const CheckboxInput = (
   props: InputHTMLAttributes<HTMLInputElement> & { indeterminate?: string }
 ): React.ReactElement => {
-  return <input type="checkbox" aria-disabled={props.disabled} {...props} />;
+  return (
+    <input
+      type="checkbox"
+      aria-disabled={props.disabled}
+      checked={props.checked}
+      onChange={props.onChange}
+      {...props}
+    />
+  );
 };
