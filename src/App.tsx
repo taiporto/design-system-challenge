b03883e01ece540ef76f44a5a4392e77b71d8773
Tai Porto <moonkoala23@gmail.com>
2023-11-12 20:07:33 -0300

    fix: fix wrong import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { TreeView } from "./components/TreeView";
-import { STORYBOOK_CHECKBOX_TREE_VIEW_DATA } from "./components/TreeView/storybook/constants";
+import { CHECKBOX_TREE_VIEW_DATA } from "./components/TreeView/__fixtures__";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
         <h2>Components</h2>
         <ul>
           <li>TreeView</li>
-          <TreeView type="checkbox" data={STORYBOOK_CHECKBOX_TREE_VIEW_DATA} />
+          <TreeView type="checkbox" data={CHECKBOX_TREE_VIEW_DATA} />
         </ul>
       </div>
     </>
